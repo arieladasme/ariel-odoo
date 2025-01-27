d61386aa87cc3aaf833557ba0ff043539a8d5fcc
ariel <ariel.wm1@gmail.com>
2025-01-27 13:44:38 -0300

    [ADD] loan.application security

--- loan_application/__manifest__.py
+++ loan_application/__manifest__.py
@@ -1,11 +1,14 @@
 {
     "name": "Solicitud de Préstamo",
     "summary": "Gestionará toda la información relevante para una solicitud de préstamo.",
-    "version": "18.0.0.1.0",
+    "version": "18.0.0.2.0",
     "category": "Technical Training/Real Estate",
     "license": "OPL-1",
     "depends": ["base"],
-    "data": [],
+    "data": [
+        'security/ir.model.access.csv',
+        'security/security_groups.xml',
+    ],
     "demo": [],
     "author": "Odoo, Inc.",
     "website": "www.odoo.com",
