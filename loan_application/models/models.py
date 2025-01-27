d39c81291bd8c0aff42cceaba7373de93fd5eee3
ariel <ariel.wm1@gmail.com>
2025-01-27 13:13:14 -0300

    [ADD] loan.application fields

--- loan_application/models/models.py
+++ loan_application/models/models.py
@@ -4,3 +4,17 @@ from odoo import fields, models
 class LoanAplication(models.Model):
     _name = "loan.application"
     _description = "Loan Application"
+
+    client = fields.Char(string="Cliente")
+    seller = fields.Char(string="Vendedor")
+    sale_order = fields.Many2one("sale.order", string="Orden de Venta")
+    state = fields.Selection([
+        ('draft', 'Borrador'),
+        ('submitted', 'Presentado'),
+        ('approved', 'Aprobado'),
+        ('rejected', 'Rechazado')
+    ], string='Estado', default='draft')
+    request_date = fields.Date(string="Fecha de Solicitud")
+    approval_date = fields.Date(string="Fecha de Aprobación")
+
+    # TODO: agregar campo etiqueta y documentos justificativos
