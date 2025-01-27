c25dfc6c49e3939e1463ef89acfbce6cc8b23247
ariel <ariel.wm1@gmail.com>
2025-01-27 12:59:30 -0300

    [ADD] loan.application model

---
+++ loan_application/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Solicitud de Préstamo",
+    "summary": "Gestionará toda la información relevante para una solicitud de préstamo.",
+    "version": "18.0.0.0.0",
+    "category": "Technical Training/Real Estate",
+    "license": "OPL-1",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "author": "Odoo, Inc.",
+    "website": "www.odoo.com",
+    "application": True,
+}
---
+++ loan_application/models/models.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+
+class LoanAplication(models.Model):
+    _name = "loan.application"
+    _description = "Loan Application"
